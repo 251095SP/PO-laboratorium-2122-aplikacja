ebc3720092b46586babf861f999be2bfc7b89fa6
251095SP <102762944+251095SP@users.noreply.github.com>
2022-05-24 14:46:59 +0200

    new commit, admin site register model

--- moto/models.py
+++ moto/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Motocykl(models.Model):
+    KM = models.IntegerField(default=0)
+    kolor = models.CharField(max_length=100)
+    waga = models.IntegerField(default=0)
+    marka = models.CharField(max_length=100)
